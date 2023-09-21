657da848a1830833361385d714bee9be795f2882
Roshan <roshanpokhrel013@gmail.com>
2023-09-21 23:31:31 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+app.listen(3000, ()=>{
+    console.log("Server is up and running!")
+})
+
+
+//console.log("Hello 4!")
