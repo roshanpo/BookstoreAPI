88de0feeea8a95a1a35486c2af5bd6c896784457
Roshan <roshanpokhrel013@gmail.com>
2023-09-24 18:44:17 +0545

    Conntected to database

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const mongoose = require('mongoose');
+
+
+const mongoURI = process.env.MONGO_URI;
+
+
+//Connecting to database
+mongoose.connect(mongoURI)
+        .then(console.log("MongoDB connected"))
+        .catch((error) => console.log(error));
 
 
 app.listen(3000, ()=>{
